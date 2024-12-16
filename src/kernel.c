ec9018cbb03a5b5d78cbab3d4dc17ce8e92e7bbb
Icosicherrys <102343489+Icosicherrys@users.noreply.github.com>
2024-12-15 20:17:02 -0600

    fixes 2

--- src/kernel.c
+++ src/kernel.c
@@ -1,13 +1,15 @@
-// MOST OF THIS IS TEMPOARY WILL BE CHANGED LATER
 typedef unsigned char uint8_t;
 typedef unsigned short uint16_t;
 typedef unsigned int uint32_t;
 typedef unsigned long uint64_t;
+
 #define UART0_BASE 0x10000000
 #define UART0_TXDATA (UART0_BASE + 0x0)
 #define UART0_TXCTRL (UART0_BASE + 0x8)
 #define NCPU 4
+
 char stack0[4096 * NCPU];
+
 #define UART_TX_READY (1 << 31)
 static inline void uart_putc(char c) {
     // Wait for the UART to be ready for transmission
@@ -16,13 +18,18 @@ static inline void uart_putc(char c) {
     // Write the character to the TXDATA register
     *(volatile uint32_t *)UART0_TXDATA = (uint32_t)c;
 }
-static void uart_puts(const char *s) {
-    while (*s) {
-        uart_putc(*s++);
-    }
-}
 int main() {
-    uart_puts("X5\n");
+    uart_putc('X');
+    uart_putc('5');
+    uart_putc('\n');
+    uart_putc('W');
+    uart_putc('e');
+    uart_putc('l');
+    uart_putc('c');
+    uart_putc('o');
+    uart_putc('m');
+    uart_putc('e');
+    uart_putc('\n');
     while (1) {
         // Loop forever
     }
